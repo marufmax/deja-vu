d631d49cc4a657363f7817a28f28465321100999
Maruf Alom <maruf.alom@portonics.com>
2020-08-17 04:56:09 +0600

    package auto discovery added

--- src/DejaVuServiceProvider.php
+++ src/DejaVuServiceProvider.php
@@ -9,13 +9,21 @@ class DejaVuServiceProvider extends ServiceProvider
     /**
      * Booting up the package
      */
-    public function boot()
+    public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->registerPublishing();
         }
+
+        $this->registerFacades();
     }
 
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/dejavu.php','dejavu'
+        );
+    }
     /**
      * Publishing package related files
      *
@@ -24,7 +32,14 @@ class DejaVuServiceProvider extends ServiceProvider
     protected function registerPublishing() : void
     {
         $this->publishes([
-            __DIR__ . '/../config/dejavu.php' => config('dejavu.php')
+            __DIR__ . '/../config/dejavu.php' => config_path('dejavu.php')
         ], 'dejavu-config');
+    }
+
+    protected function registerFacades()
+    {
+        $this->app->singleton('DejaVu', function ($app) {
+           return new \MarufMax\DejaVu\RedisCache();
+        });
     }
 }
